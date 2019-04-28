25d66995b9318a5f2564851888b6de065d587d3e
David Lau <liu1351853931@outlook.com>
2019-04-28 19:13:29 +0800

    add ansible and psh

--- os-lab环境配置.md
+++ os-lab环境配置.md
@@ -160,4 +160,3 @@ gxemul -E oldtestmips -C R3000 -M 64 -d gxemul/fs.img gxemul/vmlinux
 ```
 
 
-
---
+++ psh/psh.sh
@@ -0,0 +1,4 @@
+echo "alias pscp=parallel-scp" >> ~/.bashrc && . ~/.bashrc
+echo "alias prsync=parallel-rsync" >> ~/.bashrc && . ~/.bashrc
+echo "alias pnuke=parallel-nuke" >> ~/.bashrc && . ~/.bashrc
+echo "alias pslurp=parallel-slurp" >> ~/.bashrc && . ~/.bashrc
